e0d7bdb81ab93f22f9858812355e210b46ee5ff6
AbhishekPatil1310 <ap7497493@gmail.com>
2025-08-08 21:12:18 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "https://html-port-folio.vercel.app", // allow only your portfolio site
+  methods: ["GET", "POST"], // allowed methods
+  allowedHeaders: ["Content-Type"], // allowed headers
+}));
 app.use(express.json());
 app.use(express.static("public")); // Serve HTML/JS from public folder
 
